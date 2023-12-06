a88c4f61a3a52ba9203595eb7e8f491ad86c873f
Isaac <govegeku00@gmail.com>
2023-12-06 13:55:50 +0300

    Completed Lab

--- lib/config.py
+++ lib/config.py
@@ -2,3 +2,5 @@ import sqlite3
 
 CONN = sqlite3.connect('music.db')
 CURSOR = CONN.cursor()
+
+# The constant CONN, that is equal to a hash that contains our connection to the database as well as a constant CURSOR that allows us to interact with the database
--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,47 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    #CREATE
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        with CONN:
+            CURSOR.execute(sql)
+        CONN.commit()
+        
+
+    #SAVE
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+#the 'with' statement is used with CONN to create a database connection.
+#when the block is exited, the '__exit__' method of the connection is called,ensuring that the connection is properly closed
+#helps to avoid issues related to resource leaks or improper cleanup
+        with CONN:
+            CURSOR.execute(sql, (self.name, self.album))
+            self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
+        CONN.commit()
+                
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
+
+
+
+        
